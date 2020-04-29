e1ce8e24a89bb6d14953372592dcaf53883d7ab9
babli <babimishra002@gmail.com>
2020-04-29 14:50:21 +0530

    initial commit

--- app/src/main/java/com/example/quizb/MainActivity.java
+++ app/src/main/java/com/example/quizb/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.quizb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonEconomicQuiz = findViewById(R.id.Eco1);
+        buttonEconomicQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
+    }
+    private void startQuiz(){
+        Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/quizb/QuizContract.java
@@ -0,0 +1,4 @@
+package com.example.quizb;
+
+public final class QuizContract {
+}
