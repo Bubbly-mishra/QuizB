6499d985408184996ec18002953fc650fe83b07b
babli <babimishra002@gmail.com>
2020-04-29 16:46:17 +0530

    initial commit

--- app/src/main/java/com/example/quizb/QuizContract.java
+++ app/src/main/java/com/example/quizb/QuizContract.java
@@ -1,4 +1,19 @@
 package com.example.quizb;
 
+import android.provider.BaseColumns;
+
 public final class QuizContract {
+
+    private QuizContract() {}
+
+    public static class QuestionTable implements BaseColumns {
+        public static final String TABLE_NAME = "quiz_questions";
+        public static final String COLUMN_QUESTION = "questions";
+        public static final String COLUMN_OPTION1 = "option1";
+        public static final String COLUMN_OPTION2 = "option2";
+        public static final String COLUMN_OPTION3 = "option3";
+        public static final String COLUMN_ANSWER_NR = "answer_nr";
+
+
+    }
 }
